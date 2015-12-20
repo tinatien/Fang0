f2f7b26d3f732f3446a661c07ccaa70dae27b52c
TinaTien <tinajay8427@gmail.com>
2015-12-20 13:00:50 +0800

    homecollectionviewcell

--- Fang0/Bridging-Header.h
+++ Fang0/Bridging-Header.h
@@ -10,4 +10,5 @@
 #define Bridging_Header_h
 #import "SWRevealViewController.h"
 
+
 #endif /* Bridging_Header_h */
---
+++ Fang0/HomeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCollectionViewCell.swift
+//  Fang0
+//
+//  Created by TinaTien on 2015/12/20.
+//  Copyright © 2015年 TinaTien. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
